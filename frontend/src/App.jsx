2a79e20cb8e8dc6a59b5896ba2d50fcd2a3e42d8
Pradeepgit01 <pk0311026@gamil.com>
2026-01-19 11:19:48 +0530

    App jsx  updated

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import LogInteractionPage from "./pages/LogInteractionPage";
+
+function App() {
+  return (
+    <div style={{ padding: "24px" }}>
+      <h1>AI-First CRM – HCP Module</h1>
+      <LogInteractionPage />
+    </div>
+  );
+}
+
+export default App;
